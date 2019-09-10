21d7d608dd6481a820c9860ee434d7ccad330a38
comic-engineer-3780 <ethanjosephfournier@gmail.com>
2019-09-10 22:16:15 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,16 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  pigeon_list = {}
+  data.each_pair do |key1,value1|
+    value1.each_pair do |key2,value2|
+      value2.each do |pigeon|
+        if !pigeon_list[pigeon]
+          pigeon_list[pigeon] = {key1 => ["#{key2}"]}
+        # else
+        #   pigeon_list[pigeon][key1].push key2
+        end
+      end
+    end
+  end
+  pigeon_list
 end
---
+++ practice.rb
@@ -0,0 +1,37 @@
+def nyc_pigeon_organizer(data)
+  # write your code here!
+  pigeon_list = {}
+  data.each_pair do |key1,value1|
+    value1.each_pair do |key2,value2|
+      value2.each do |pigeon|
+        if !pigeon_list[pigeon]
+          pigeon_list[pigeon] = {key1 => ["#{key2}"]}
+        else
+          pigeon_list[pigeon][key1].push(key2.to_s)
+        end
+      end
+    end
+  end
+  puts pigeon_list
+end
+
+pigeon_data = {
+  :color => {
+    :purple => ["Theo", "Peter Jr.", "Lucky"],
+    :grey => ["Theo", "Peter Jr.", "Ms. K"],
+    :white => ["Queenie", "Andrew", "Ms. K", "Alex"],
+    :brown => ["Queenie", "Alex"]
+  },
+  :gender => {
+    :male => ["Alex", "Theo", "Peter Jr.", "Andrew", "Lucky"],
+    :female => ["Queenie", "Ms. K"]
+  },
+  :lives => {
+    "Subway" => ["Theo", "Queenie"],
+    "Central Park" => ["Alex", "Ms. K", "Lucky"],
+    "Library" => ["Peter Jr."],
+    "City Hall" => ["Andrew"]
+  }
+}
+
+nyc_pigeon_organizer(pigeon_data)
