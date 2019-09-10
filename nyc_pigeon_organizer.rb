caaa49729077a40a49172de518eb4412855bf537
comic-engineer-3780 <ethanjosephfournier@gmail.com>
2019-09-10 22:24:24 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,17 +1,17 @@
-def nyc_pigeon_organizer(data)
-  # write your code here!
-  pigeon_list = {}
-  data.each do |key1,value1|
-    value1.each do |key2,value2|
-      value2.each do |pigeon|
-        if !pigeon_list[pigeon]
-          pigeon_list[pigeon] = {}
-        else
-          pigeon_list[pigeon][key1] = []
-        end
-        pigeon_list[pigeon][key1] << value2.to_s
-      end
-    end
-  end
-  pigeon_list
-end
+# def nyc_pigeon_organizer(data)
+#   # write your code here!
+#   pigeon_list = {}
+#   data.each do |key1,value1|
+#     value1.each do |key2,value2|
+#       value2.each do |pigeon|
+#         if !pigeon_list[pigeon]
+#           pigeon_list[pigeon] = {}
+#         else
+#           pigeon_list[pigeon][key1] = []
+#         end
+#         pigeon_list[pigeon][key1] << value2.to_s
+#       end
+#     end
+#   end
+#   pigeon_list
+# end
