8c0283aa18efb09d41b970692b223d9bdacba54b
comic-engineer-3780 <ethanjosephfournier@gmail.com>
2019-09-10 22:21:22 +0000

    Automatically backed up by Learn

--- practice.rb
+++ practice.rb
@@ -1,8 +1,8 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
   pigeon_list = {}
-  data.each_pair do |key1,value1|
-    value1.each_pair do |key2,value2|
+  data.each do |key1,value1|
+    value1.each do |key2,value2|
       value2.each do |pigeon|
         if !pigeon_list[pigeon]
           pigeon_list[pigeon] = {}
